ca1b44921f1715a4feaedda8a60a694820963672
andy kim <kst6294@.com>
2020-10-26 00:14:43 +0900

    게시판 내용과 mysql 연동 완료, 로그인 기능 및 검색 기능 구현 필요

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, redirect
+from flask.helpers import url_for
+import datetime
 import pymysql
 
 
@@ -13,15 +15,28 @@ db = pymysql.connect(
 )
 cur = db.cursor()
 
-sql = "SELECT * from board"
-cur.execute(sql)
 
-data_list = cur.fetchall()
-
-
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def home():
-    return render_template("index.html", data_list=data_list)
+    if request.method == 'GET':
+        sql = "SELECT * from board"
+        cur.execute(sql)
+        data_list = cur.fetchall()
+        return render_template("index.html", data_list=data_list)
+    elif request.method == 'POST':
+        board_title = request.form['board_title']
+        board_text = request.form['board_text']
+        board_writer = request.form['board_writer']
+        board_submit = datetime.datetime.now().strftime("%Y-%m-%d")
+        sql = "INSERT INTO board (board_title, board_text, board_writer, board_submit) VALUES (%s, %s, %s, %s);"
+        cur.execute(sql, (board_title, board_text, board_writer, board_submit))
+        db.commit()
+    return redirect(url_for('home'))
+
+
+@ app.route("/write", methods=['GET', 'POST'])
+def write():
+    return render_template("write.html")
 
 
 if __name__ == "__main__":
