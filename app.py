cf4d69ed772c142cd49d8590d27a3d473fcab41c
andy kim <kst6294@gmail.com>
2020-10-30 01:11:15 +0900

    게시판 클릭시에 상세페이지 보기 연동 예정

--- app.py
+++ app.py
@@ -18,37 +18,54 @@ db = pymysql.connect(
 cur = db.cursor()
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/")
 def home():
-    if request.method == 'GET':
-        sql = "SELECT * from board"
-        cur.execute(sql)
-        data_list = cur.fetchall()
-        return render_template("index.html", data_list=data_list)
-    elif request.method == 'POST':
-        board_title = request.form['board_title']
-        board_text = request.form['board_text']
-        board_writer = request.form['board_writer']
-        board_submit = datetime.datetime.now().strftime("%Y-%m-%d")
-        sql = "INSERT INTO board (board_title, board_text, board_writer, board_submit) VALUES (%s, %s, %s, %s);"
-        cur.execute(sql, (board_title, board_text, board_writer, board_submit))
-        db.commit()
-    return redirect(url_for('home'))
+    sql = "SELECT * FROM board"
+    cur.execute(sql)
+    data_list = cur.fetchall()
+    return render_template("index.html", data_list=data_list)
 
 
 @app.route("/write", methods=['GET', 'POST'])
 def write():
-    return render_template("write.html")
+    if request.method == 'POST':
 
+        # post method 를 통해 write 페이지 접속 후, form 데이터를 받아야 함
+        board_title = request.form['board_title']
+        board_text = request.form['board_text']
+        board_writer = request.form['board_writer']
+        board_submit = datetime.datetime.now().strftime("%Y-%m-%d")
 
-# @app.route("/view", methods=['GET', 'POST'])
-# def view():
-#     return render_template("view.html")
+        # 데이터베이스 목록 가져오기
+        sql = "INSERT INTO board (board_title, board_text, board_writer, board_submit) VALUES (%s, %s, %s, %s);"
+        cur.execute(sql, (board_title, board_text, board_writer, board_submit))
+        db.commit()
 
+        # 마지막으로 insert 한 데이터메이서 primary key 가져오기
+        sql = "SELECT last_insert_id()"
+        last_row_id = cur.lastrowid
+        find_one = "SELECT * FROM board WHERE id = %d" % last_row_id
 
-@app.route("/login", methods=['GET', 'POST'])
-def view():
-    return render_template("login.html")
+        # title
+        cur.execute(find_one)
+        title = cur.fetchone()[1]
+
+        # contents
+        cur.execute(find_one)
+        contents = cur.fetchone()[2]
+
+        # writer
+        cur.execute(find_one)
+        writer = cur.fetchone()[3]
+
+        # date
+        cur.execute(find_one)
+        sub_date = cur.fetchone()[4]
+
+        return render_template("view.html", sub_date=sub_date, title=title, contents=contents, writer=writer)
+
+    else:
+        return render_template("write.html")
 
 
 if __name__ == "__main__":
