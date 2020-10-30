d8d6fc96da3ea25039da3059da6c7b6e460e45c2
andy kim <kst6294@gmail.com>
2020-10-30 23:29:00 +0900

    페이지네이션 구현해야함

--- app.py
+++ app.py
@@ -20,10 +20,25 @@ cur = db.cursor()
 
 @app.route("/")
 def home():
-    sql = "SELECT * FROM board"
+    sql = "SELECT * FROM board LIMIT 0, 10"
     cur.execute(sql)
     data_list = cur.fetchall()
-    return render_template("index.html", data_list=data_list)
+
+    # 페이지의 값을 가져와서 리스트를 업데이트 시켜줘야함.
+    page_num = request.args.get("page")
+    if page_num is not None:
+        page_num = int(page_num)
+
+        # limit 시작 과 끝 데이터를 호출
+        limit_start = (page_num - 1) * 10
+        limit_end = page_num * 10
+        # page 를 눌렀을 때 각 page 값을 호출 한 후에 쿼리 작성
+        sql = f"SELECT * FROM board LIMIT %d, %d" % (limit_start, limit_end)
+        cur.execute(sql)
+        data_list = cur.fetchall()
+        return render_template("index.html", data_list=data_list)
+    else:
+        return render_template("index.html", data_list=data_list)
 
 
 @app.route("/write", methods=['GET', 'POST'])
@@ -66,6 +81,29 @@ def write():
 
     else:
         return render_template("write.html")
+
+
+@app.route("/view", methods=['GET', 'POST'])
+def view():
+    idx = request.args.get("id")
+    find_one = f"SELECT * FROM board WHERE id = {idx}"
+    cur.execute(find_one)
+
+    title = cur.fetchone()[1]
+
+    # contents
+    cur.execute(find_one)
+    contents = cur.fetchone()[2]
+
+    # writer
+    cur.execute(find_one)
+    writer = cur.fetchone()[3]
+
+    # date
+    cur.execute(find_one)
+    sub_date = cur.fetchone()[4]
+
+    return render_template("view.html", sub_date=sub_date, title=title, contents=contents, writer=writer)
 
 
 if __name__ == "__main__":
